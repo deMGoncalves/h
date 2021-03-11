d2bdecb343a824062559da1ed5d93a6c52dd5f50
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-11 08:56:49 -0300

    style(slot): nao preciso do curry, pois nao usamos este recurso aqui

--- src/slot/slot.js
+++ src/slot/slot.js
@@ -1,9 +1,7 @@
 import * as f from 'f'
 
-const slot = (children) =>
+export default (children) =>
   new Proxy(children, {
     get: (target, name) =>
       f.or(target[name], f.find(target, (child) => f.equal(name, f.prop('slot', child))))
   })
-
-export default f.curry(slot)
