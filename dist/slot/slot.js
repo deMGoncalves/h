69662b43d8b81224af3597dadd94eccd0a053c12
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-17 19:01:26 -0300

    build(dist): automatic new publish

--- dist/slot/slot.js
+++ dist/slot/slot.js
@@ -1,5 +1,5 @@
 import * as f from 'f';
 const slot = (children) => new Proxy(children, {
-    get: (target, name) => f.or(target[name], f.find(target, (child) => f.equal(name, f.prop('slot', child))))
+    get: (target, name) => f.or(target[name], f.filter(target, (child) => f.equal(name, f.prop('slot', child))))
 });
 export default f.curry(slot);
