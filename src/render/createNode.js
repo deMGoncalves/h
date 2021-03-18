4f55f1a4118f772cc7db7ca0738951384388108f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-18 10:43:01 -0300

    feat(render): incluindo o set attribute na criacao do node

--- src/render/createNode.js
+++ src/render/createNode.js
@@ -1,5 +1,7 @@
+import * as f from 'f'
 import appendChildren from './appendChildren'
 import createElement from './createElement'
+import setAttributes from './setAttributes'
 
 export default (descriptor) =>
-  appendChildren(createElement(descriptor), descriptor)
+  f.compose(appendChildren(descriptor), setAttributes(descriptor))(createElement(descriptor))
