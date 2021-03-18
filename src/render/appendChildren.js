a49cbaf6a18dd72ce372086c17e3b2f0a9e0972e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-18 10:42:12 -0300

    style(render): colocando o append children dentro do curry

--- src/render/appendChildren.js
+++ src/render/appendChildren.js
@@ -1,5 +1,7 @@
 import * as f from 'f'
 import create from './create'
 
-export default (parent, descriptor) =>
+const appendChildren = (descriptor, parent) =>
   f.always(parent)(parent.append(...f.map(descriptor.children, create)))
+
+export default f.curry(appendChildren)
