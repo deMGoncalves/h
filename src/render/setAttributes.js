ab40d81120919c96203eb8e79f5e3c292514e862
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-23 08:26:21 -0300

    style(render): simplificando a passagem de parametros

--- src/render/setAttributes.js
+++ src/render/setAttributes.js
@@ -1,7 +1,7 @@
 import * as f from 'f'
 
 const setAttibutes = (descriptor, parent) =>
-  f.always(parent)(f.forEach(f.toPairs(descriptor.attributes), ([key, value]) =>
-    parent.setAttribute(key, value)))
+  f.always(parent)(f.forEach(f.toPairs(descriptor.attributes), (args) =>
+    parent.setAttribute(...args)))
 
 export default f.curry(setAttibutes)
