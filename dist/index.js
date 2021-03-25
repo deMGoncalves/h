3f5dd308874d1560c4abc688f152dbd4883b356b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-25 10:11:52 -0300

    refactor(h): show e hide estao ok para a nova forma de se trabalhar com o fragment

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
 export { default } from "./h/index";
 export { default as Fragment } from "./fragment/index";
-// export { default as Hide } from './hide'
+export { default as Hide } from "./hide/index";
 // export { default as paint } from './paint'
 // export { default as Raw } from './raw'
 export { default as render } from "./render/index";
 // export { default as Repeat } from './repeat'
 // export { default as repaint } from './repaint'
-// export { default as Show } from './show'
+export { default as Show } from "./show/index";
 // export { default as Zone } from './zone'
