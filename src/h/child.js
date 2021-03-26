1054cc7d9bd4343f16f5144566eaf47fedc4e498
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-26 15:07:57 -0300

    refactor(child): usando o f para os processos funcionais

--- src/h/child.js
+++ src/h/child.js
@@ -1,12 +1,15 @@
+import * as f from 'f'
 import TextElement from './textElement'
 
 class Child {
   static create (children, parent) {
-    return children
-      .map((nodeOrText) =>
-        (nodeOrText instanceof Object)
-          ? nodeOrText.setParent(parent)
-          : TextElement.create(nodeOrText, parent))
+    return f.map(
+      children,
+      (nodeOrText) =>
+        f.or(f.is(String, nodeOrText), f.is(Number, nodeOrText))
+          ? TextElement.create(nodeOrText, parent)
+          : nodeOrText.setParent(parent)
+    )
   }
 }
 
