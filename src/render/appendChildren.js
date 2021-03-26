3bdbb9fe3da56ae188b12ca9b5cad7c0516d1ffc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-26 16:00:05 -0300

    refactor(nodemapper): trocando o nome para element mapper

--- src/render/appendChildren.js
+++ src/render/appendChildren.js
@@ -1,7 +1,7 @@
 import * as f from 'f'
-import nodeMapper from './nodeMapper'
+import elementMapper from './elementMapper'
 
 const appendChildren = (descriptor, parent) =>
-  f.always(parent)(parent.append(...f.map(descriptor.children, nodeMapper)))
+  f.always(parent)(parent.append(...f.map(descriptor.children, elementMapper)))
 
 export default f.curry(appendChildren)
--- src/render/render.js
+++ src/render/render.js
@@ -1,7 +1,7 @@
 import * as f from 'f'
-import nodeMapper from './nodeMapper'
+import elementMapper from './elementMapper'
 
 const render = (element, rootElement) =>
-  element.append(nodeMapper(rootElement))
+  element.append(elementMapper(rootElement))
 
 export default f.curry(f.arity(2, f.frame(render)))
