3f2f16118638b1a1c48a1ebc3396993c986e8af5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-28 15:53:38 -0300

    refactor(children): abrindo o get parent e node

--- src/tag/children.js
+++ src/tag/children.js
@@ -2,17 +2,26 @@ import Text from '@/text'
 
 class Children {
   #nodes
+  #parent
 
-  constructor (nodes) {
+  get nodes () {
+    return this.#nodes
+  }
+
+  get parent () {
+    return this.#parent
+  }
+
+  constructor (nodes, parent) {
     this.#nodes = nodes
+    this.#parent = parent
   }
 
   static create (nodes, parent) {
     return new Children(
       f.map(nodes, (textOrTag) =>
-        Text.is(textOrTag)
-          ? Text.create(textOrTag, parent)
-          : textOrTag.setParent(parent))
+        Text.is(textOrTag) ? Text.create(textOrTag, parent) : textOrTag.setParent(parent)),
+      parent
     )
   }
 }
