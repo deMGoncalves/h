5f87e3269962bc1185012da4fcb8db81310f8941
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-28 14:48:38 -0300

    feat(tag): metodo statico para criar um component

--- src/tag/tag.js
+++ src/tag/tag.js
@@ -1,6 +1,10 @@
 import * as f from 'f'
 
 class Tag {
+  static create (tagName, props, children) {
+    return new Tag(tagName, props, children)
+  }
+
   static is (tagNameOrComponent) {
     return f.is(String, tagNameOrComponent)
   }
