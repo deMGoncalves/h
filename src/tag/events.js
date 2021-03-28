df4ccd03c698ae8b87aaf756fc5f1d513d884fa6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-28 20:56:48 -0300

    feat(events): estrutura base para o controle dos eventos

--- src/tag/events.js
+++ src/tag/events.js
@@ -1,5 +1,28 @@
-class Events {
+import * as f from 'f'
 
+class Events {
+  #list
+  #parent
+
+  get list () {
+    return f.map(this.#list, ([name, listener]) => ({ name, listener }))
+  }
+
+  get parent () {
+    this.#parent
+  }
+
+  constructor (list, parent) {
+    this.#list = list
+    this.#parent = parent
+  }
+
+  static create (props, parent) {
+    return new Events (
+      f.filter(f.entries(props), f.compose(f.test(/^on\S+$/), f.prop('[0]'))),
+      parent
+    )
+  }
 }
 
 export default Events
