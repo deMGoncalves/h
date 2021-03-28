6c9fc451b2c9b35c5f4f328df6fa9bede64f5371
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-28 14:34:23 -0300

    feat(component): metodo statico para executar um function component

--- src/component/component.js
+++ src/component/component.js
@@ -1,6 +1,10 @@
 import * as f from 'f'
 
 class Component {
+  static execute (componentRef, props, children) {
+    return componentRef(props, slot(children)) 
+  }
+
   static is (tagNameOrComponent) {
     return f.is(Function, tagNameOrComponent)
   }
