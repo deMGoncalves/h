c7a6cbfc1dd22388571a58d5c7bc6dce4d72e82b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-28 14:44:00 -0300

    fix(component): children dever contar apenas os filhos sem um slot

--- src/component/slot.js
+++ src/component/slot.js
@@ -2,7 +2,7 @@ import * as f from 'f'
 
 export default (children) =>
   new Proxy(
-    f.filter(children, f.has('slot')),
+    f.filter(children, f.compose(f.not, f.has('slot'))),
     {
       get: (target, slotName) =>
         f.or(target[slotName], f.filter(children, f.compose(f.equal(slotName), f.prop('slot'))))
