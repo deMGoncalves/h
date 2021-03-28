c3f2239178d85b48f69229ab570c760db4a66a59
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-28 14:59:12 -0300

    feat(classname): abrindo o atributo value ja tratado os multiplos valores

--- src/tag/className.js
+++ src/tag/className.js
@@ -1,8 +1,18 @@
 import * as f from 'f'
 
 class ClassName {
+  #value
+
+  get value () {
+    return this.#value
+  }
+
+  constructor (value) {
+    this.#value = value
+  }
+
   static create (props) {
-    return new ClassName(f.join(f.flatten(f.concat([], props.className)), ' '))
+    return new ClassName(f.join(f.flatten(f.concat([], f.or(props.className, ''))), ' '))
   }
 }
 
