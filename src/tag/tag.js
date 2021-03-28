2433c533f5d66fa4086510c311baf35d12fa2563
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-28 14:50:53 -0300

    feat(tag): logica para o tag name

--- src/tag/tag.js
+++ src/tag/tag.js
@@ -1,6 +1,16 @@
 import * as f from 'f'
 
 class Tag {
+  #tagName
+
+  get tagName () {
+    return this.#tagName
+  }
+
+  constructor (tagName, props, children) {
+    this.#tagName = tagName
+  }
+
   static create (tagName, props, children) {
     return new Tag(tagName, props, children)
   }
