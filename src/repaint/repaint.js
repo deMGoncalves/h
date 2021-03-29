cf45372c03116304be309456f86c30e8727130b0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-29 12:48:21 -0300

    feat(repaint): este decorator atualiza o dom com o novo estado da entidade

---
+++ src/repaint/repaint.js
@@ -0,0 +1,16 @@
+import * as f from 'f'
+
+const repaint = function (_entity, _method, descriptor) {
+  const method = descriptor.value
+
+  return f.assign(
+    descriptor,
+    {
+      value: function () {
+        return f.always(method.apply(this, arguments))(this[f.magic('repaint')]())
+      }
+    }
+  )
+}
+
+export default repaint
