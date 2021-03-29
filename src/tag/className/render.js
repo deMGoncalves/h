92f125f994f7ec7f6ef54c3281baac000a0cfe27
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-29 11:13:19 -0300

    feat(classname): so se deve setar o class name se tiver valor

--- src/tag/className/render.js
+++ src/tag/className/render.js
@@ -4,7 +4,7 @@ import hook from 'hook'
 const render = (className) =>
   f.assign(className, {
     [f.magic('render')]: (element) =>
-      (element.className = className.value)
+      (className.value && (element.className = className.value))
   })
 
 export default hook(render)
