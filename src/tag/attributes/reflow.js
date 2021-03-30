657d968cd79d924b09023c6c37d619c9eda98644
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-30 08:12:21 -0300

    fix(attributes): estilizacao do codigo

--- src/tag/attributes/reflow.js
+++ src/tag/attributes/reflow.js
@@ -9,9 +9,9 @@ const reflow = (attributes) =>
         f.zip(attributes.list, newAttributes.list),
         ([attribute, newAttribute]) => (
           f.and(
-            f.equal(attribute.key, newAttribute.value),
+            f.equal(attribute.key, newAttribute.key),
             f.different(attribute.value, newAttribute.value)
-          ) && attributes.set(attribute.key, attribute.value),
+          ) && attributes.set(attribute.key, newAttribute.value),
 
           f.different(attribute.key, newAttribute.key) && (
             attributes.remove(attributes.key),
