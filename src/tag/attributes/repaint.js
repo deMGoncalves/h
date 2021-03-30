e8da2addd35ddb210a6d3690e7e0975a1b550567
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-30 08:00:51 -0300

    fix(repaint): deve retornar o text

---
+++ src/tag/attributes/repaint.js
@@ -0,0 +1,8 @@
+import * as f from 'f'
+import { before } from 'hook'
+import dom from '@/dom'
+
+const repaint = (attributes) =>
+  ((dom.query(attributes.parent.id).setAttribute()= text.content), text)
+
+export default after(f.frame(repaint))
