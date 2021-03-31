8376529de31f8f8c001de24f03cd96e68e927948
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-31 14:08:53 -0300

    refactor(repaint): nao preciso trabalhar com variavel

--- dist/repaint/repaint.js
+++ dist/repaint/repaint.js
@@ -1,7 +1,7 @@
 import * as f from 'f';
 import { after } from 'hook';
-const repaint = function (returned) {
+function repaint(returned) {
     f.or(this[f.magic('reflow')], f.always())();
     return returned;
-};
+}
 export default after(repaint);
