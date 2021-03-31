0264af3cd277b2b4ac9a7fa322c232975d0ef080
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-31 17:20:37 -0300

    feat(render): encapsulando o render com o curry

--- dist/render/render.js
+++ dist/render/render.js
@@ -1,3 +1,3 @@
 import * as f from 'f';
 const render = (element, node) => element.append(node[f.magic('paint')]());
-export default f.frame(render);
+export default f.curry(f.arity(2, f.frame(render)));
--- src/render/render.js
+++ src/render/render.js
@@ -3,4 +3,4 @@ import * as f from 'f'
 const render = (element, node) =>
   element.append(node[f.magic('paint')]())
 
-export default f.frame(render)
+export default f.curry(f.arity(2, f.frame(render)))
