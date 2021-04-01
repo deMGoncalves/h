b5ae1584ccf33a11db4595387a467639d30ca4fc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-01 17:56:05 -0300

    fix(fragment): estou com problemas com o fragment

--- src/tag/children/repaint/replace.js
+++ src/tag/children/repaint/replace.js
@@ -3,12 +3,7 @@ import { before } from 'hook'
 import dom from '@/dom'
 
 function replace (child, other) {
-  console.log(child, other)
-  const element = f.is(Function, other)
-    ? other()
-    : other[f.magic('paint')]()
-
-  dom.query(child.id).parentNode.replaceChild(element, dom.query(child.id))
+  dom.query(child.id).parentNode.replaceChild(other[f.magic('paint')], dom.query(child.id))
   return [child, other]
 }
 
